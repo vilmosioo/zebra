273b6bebc9fe0606d628133abf2b9cea5660774a
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-02-25 19:56:42 +0000

    Add eslint and blueprint and calendar heatmap

--- packages/zebra-app/src/App.tsx
+++ packages/zebra-app/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CalendarHeatmap from "react-calendar-heatmap";
 
 function App() {
+  const values = [{
+    date: new Date(),
+    value: 1
+  }];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        Zebra
       </header>
+      <div>
+        <CalendarHeatmap values={values} />
+      </div>
     </div>
   );
 }
