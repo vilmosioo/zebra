fcfca46971c2943c8ea24ecf2fb614518392ce9d
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-03-10 20:21:35 +0000

    :/

--- packages/zebra-app/src/App.test.tsx
+++ packages/zebra-app/src/App.test.tsx
@@ -3,7 +3,5 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test.skip("renders zebra title", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/zebra/i);
-  expect(linkElement).toBeInTheDocument();
+  // todo
 });
