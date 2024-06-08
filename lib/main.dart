bd953125526cd4f0e018e7524790f07312394a66
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-06-08 14:37:33 +0100

    Calendar heatmap (#7)
    * heatmap
    
    * radius

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 import 'globals.dart' as globals;
 
 void main() {
@@ -43,11 +44,31 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: globals.mainColor,
       ),
       body: Center(
-        child: Text('Please upload your Finch data.'),
+        child: HeatMapCalendar(
+          defaultColor: Colors.white,
+          flexible: true,
+          colorMode: ColorMode.color,
+          datasets: {
+            DateTime(2024, 6, 6): 1,
+            DateTime(2024, 6, 7): 1,
+            DateTime(2024, 6, 8): 1,
+            DateTime(2024, 6, 9): 1,
+            DateTime(2024, 6, 13): 1,
+          },
+          size: 12,
+          borderRadius: 50,
+          margin: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 10
+          ),
+          colorsets: const {
+            1: globals.mainColor,
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ["json"], );
+          FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ["zip"], );
 
           if (result != null) {
             PlatformFile file = result.files.first;
