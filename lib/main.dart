e64606c74a5ada152b64ebd540f542509f56a286
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-06-15 13:45:56 +0100

    Read list of goals (#12)
    * Read list of goals
    
    * what
    
    * clean
    
    * some ui
    
    * more ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'dart:io';
+import 'dart:convert';
 
+import 'package:archive/archive.dart';
 import 'package:english_words/english_words.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_archive/flutter_archive.dart';
+// import 'package:flutter_archive/flutter_archive.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 
-import 'globals.dart' as globals;
 import 'select.dart';
 
 void main() {
@@ -25,8 +25,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Zebra',
         theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: globals.mainColor),
+          primaryColor: const Color.fromARGB(100, 65, 180, 255),
         ),
         home: const MyHomePage(),
       ),
@@ -46,14 +45,13 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Zebra'),
-        backgroundColor: globals.mainColor,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const GoalSelector(),
           HeatMapCalendar(
             defaultColor: Colors.white,
-            flexible: true,
             colorMode: ColorMode.color,
             datasets: {
               DateTime(2024, 6, 6): 1,
@@ -62,46 +60,65 @@ class MyHomePage extends StatelessWidget {
               DateTime(2024, 6, 9): 1,
               DateTime(2024, 6, 13): 1,
             },
-            size: 12,
-            borderRadius: 50,
+            size: 50,
+            borderRadius: 100,
+            showColorTip: false,
             margin: const EdgeInsets.symmetric(
               vertical: 10,
               horizontal: 10
             ),
             colorsets: const {
-              1: globals.mainColor,
+              1: Color.fromARGB(100, 65, 180, 255),
             },
           ),
         ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ["zip"], );
+          FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ["zip"], withData: true );
 
-          if (result != null) {
-            File file = File(result.files.single.path!);
-
-            final destinationDir = await Directory.systemTemp.createTemp();
-            try {
-              await ZipFile.extractToDirectory(zipFile: file, destinationDir: destinationDir);
-              
-              Fluttertoast.showToast(
-                msg: destinationDir.listSync().length.toString(),
-                toastLength: Toast.LENGTH_SHORT,
-                gravity: ToastGravity.CENTER,
-                timeInSecForIosWeb: 1,
-                backgroundColor: globals.mainColor,
-                fontSize: 16.0
-              );
-            } catch (e) {
-              Fluttertoast.showToast(
-                msg: "Failed to extract zip file",
-                toastLength: Toast.LENGTH_LONG,
-                gravity: ToastGravity.CENTER,
-                timeInSecForIosWeb: 1,
-                backgroundColor: globals.mainColor,
-                fontSize: 16.0
-              );
+          if (result != null && result.files.isNotEmpty) {
+            final bytes = result.files.first.bytes?.toList();
+            if (bytes != null) {
+              try {
+                final archive = ZipDecoder().decodeBytes(bytes);
+                
+                for (final file in archive) {
+                  if (file.isFile && file.name == "Bullet.json") {
+                    final goalBytes = file.content as List<int>;
+                    final goalRaw = utf8.decode(goalBytes);
+                    final goals = (jsonDecode(goalRaw) as Map<String, dynamic>)['data'];
+                    // const goals = JSON.parse(await goalFile?.getData?.(goalWriter) ?? "{\"data\": []}")?.data as IRawBullet[];
+                    // try {
+                    //   const goalFile = contents.find(content => content.filename === "Bullet.json");
+                    //   const goals = JSON.parse(await goalFile?.getData?.(goalWriter) ?? "{\"data\": []}")?.data as IRawBullet[];
+                    //   setGoals(goals.filter(g => g.bullet_type === 1).map(m => ({
+                    //     date: new Date(m.dt),
+                    //     name: m.text,
+                    //     isCompleted: m.completed_time != "",
+                    //     // should be 5 but it's not a nice color
+                    //     value: m.completed_time != "" ? "4" : "1"
+                    //   })));
+                    // } catch(err) {
+                    //   console.error(err);
+                    // }
+                    Fluttertoast.showToast(
+                      msg: goals.toString(),
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      fontSize: 16.0,
+                    );
+                  }
+                }
+              } catch (e) {
+                Fluttertoast.showToast(
+                  msg: "Failed to extract zip file$e",
+                  toastLength: Toast.LENGTH_LONG,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  fontSize: 16.0
+                );
+              }
             }
           } else {
             // User canceled the picker
