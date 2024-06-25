421e3af5b032aca2c1b94326b74ce6868d2adac6
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-06-25 12:48:06 +0100

    fix unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,33 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_test/hive_test.dart';
+import 'package:zebra/common/constants.dart';
 
 import 'package:zebra/main.dart';
 
 void main() {
+  Box? box;
+
+  setUp(() async {
+    await setUpTestHive();
+    box = await Hive.openBox(zebraBox);
+    // todo add some testing data
+    // have to put the call inside a `runAsync()`
+    // await tester.runAsync(() => box.put('key', 'hello world'));
+    await box?.clear();
+  });
+
+  tearDown(() async {
+    await box?.deleteFromDisk();
+    await tearDownTestHive();
+  });
+
   testWidgets('Basic scaffolding', (WidgetTester tester) async {
     await tester.pumpWidget(const ZebraApp());
 
