3aaf876987521b09e297bf72a50f4e1c7bea3225
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-06-30 18:49:02 +0100

    calendar

---
+++ lib/widgets/heatmap_calendar.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../common/constants.dart';
+import '../model/goals.dart';
+
+// Thu, 9 Nov 2023 01:00:00
+DateFormat format = DateFormat("E, d LLL y");
+
+/// Widget to display a calendar heatmap of a specific goal.
+class GoalsHeatMapCalendar extends StatelessWidget {
+  const GoalsHeatMapCalendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<GoalsModel>(
+      builder: (context, model, child) {
+        return ValueListenableBuilder(
+          valueListenable: Hive.box(zebraBox).listenable(),
+          builder: (context, box, widget) {
+            // Render empty box when list of goals is empty.
+            if (box.isEmpty) {
+              return const SizedBox();
+            }
+            // Render empty box when no goal is selected.
+            if (model.selectedGoal == null) {
+              return const SizedBox();
+            }
+            final selectedGoal = box.get(model.selectedGoal);
+            if (selectedGoal == null) {
+              throw "Selected goal does not exist";
+            }
+            final datasets = <DateTime, int>{};
+            for (var report in selectedGoal) {
+              datasets[format.parse(report.date)] = report.isCompleted ? 1 : 0;
+            }
+            final max = datasets.keys.reduce((a,b) => a.isAfter(b) ? a : b);
+            return HeatMapCalendar(
+              defaultColor: Colors.white,
+              colorMode: ColorMode.color,
+              datasets: datasets,
+              initDate: max,
+              size: 40,
+              borderRadius: 100,
+              showColorTip: false,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5,
+                horizontal: 5
+              ),
+              colorsets: const {
+                0: Color.fromARGB(50, 255, 0, 0),
+                1: Color.fromARGB(100, 65, 180, 255),
+              },
+            );
+          }
+        );
+      }
+    );
+  }
+}
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import '../model/goals.dart';
 import 'goal_selector.dart';
 import 'heatmap.dart';
+import 'heatmap_calendar.dart';
 import 'upload_button.dart';
 
 class ZebraHomePage extends StatelessWidget {
@@ -23,6 +24,7 @@ class ZebraHomePage extends StatelessWidget {
           children: [
             GoalSelector(),
             GoalsHeatMap(),
+            GoalsHeatMapCalendar(),
           ]
         ),
         floatingActionButton: const UploadButton(),
