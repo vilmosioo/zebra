6fee9155e1d8ba28aae5fee2bdfa92a33c2688a8
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-07-05 18:00:48 +0100

    undo selector

--- lib/widgets/goal_selector.dart
+++ lib/widgets/goal_selector.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_select/flutter_native_select.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:zebra/model/goals.dart';
@@ -7,8 +6,15 @@ import 'package:zebra/model/goals.dart';
 import '../common/constants.dart';
 
 /// Widget to display a selector for a list of goals.
-class GoalSelector extends StatelessWidget {
+class GoalSelector extends StatefulWidget {
   const GoalSelector({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _GoalSelectorState();
+}
+
+class _GoalSelectorState extends State<GoalSelector> {
+  String? dropdownValue;
   
   @override
   Widget build(BuildContext context) {
@@ -17,57 +23,43 @@ class GoalSelector extends StatelessWidget {
         return ValueListenableBuilder(
           valueListenable: Hive.box(zebraBox).listenable(),
           builder: (context, box, widget) {
-            // todo remove cast
             final goals = box.toMap();
-            return _GoalSelectorInternal(goals, model.selectedGoal, model.setSelectedGoal);
+            if (goals.isEmpty) {
+              // Render empty box when list of goals is empty.
+              return const SizedBox();
+            }
+            final List<String> keys = List.from(goals.keys);
+            // Sort keys by number of reports.
+            keys.sort((a, b) {
+              final aGoal = goals[a]!;
+              final bGoal = goals[b]!;
+              return bGoal.length - aGoal.length;
+            });
+            return LayoutBuilder(builder: (context, constraints) {
+              return DropdownMenu<String>(
+                width: constraints.maxWidth - 20, // <-- This is necessary to force the menu items to the dropdown width.
+                expandedInsets: const EdgeInsets.all(10), // <-- This is necessary to make the dropdown menu full with with some margin.
+                dropdownMenuEntries: keys.map<DropdownMenuEntry<String>>((String key) {
+                  final String labelText = "${key.replaceAll("#", "")} (${goals[key]?.length})";
+                  return DropdownMenuEntry<String>(
+                    value: key,
+                    label: labelText,
+                    labelWidget: Text(
+                        labelText,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                  );
+                }).toList(),
+                initialSelection: dropdownValue,
+                onSelected: (String? value) {
+                  model.setSelectedGoal(value);
+                }
+              );
+            });
           }
         );
       }
     );
   }
-}
-class _GoalSelectorInternal extends StatelessWidget {
-  final Map<dynamic, dynamic> goals;
-  final String? selectedGoal;
-  final void Function(String?) onSelected;
-
-  const _GoalSelectorInternal(this.goals, this.selectedGoal, this.onSelected);
-
-  @override
-  Widget build(BuildContext context) {
-      if (goals.isEmpty) {
-        // Render empty box when list of goals is empty.
-        return const SizedBox();
-      }
-      final List<String> keys = List.from(goals.keys);
-      // Sort keys by number of reports.
-      keys.sort((a, b) {
-        final aGoal = goals[a]!;
-        final bGoal = goals[b]!;
-        return bGoal.length - aGoal.length;
-      });
-      return ElevatedButton.icon(
-        onPressed: () async {
-          final s = await _showGoalSelect(keys);
-          onSelected(s);
-        },
-        style: ElevatedButton.styleFrom(
-          minimumSize: const Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
-        ),
-        icon: const Icon(Icons.expand_more_outlined),
-        iconAlignment: IconAlignment.end,
-        label: Text(selectedGoal?.replaceAll("#", "") ?? "Please select a goal"),
-      );
-  }
-}
-
-Future<String?> _showGoalSelect(List<String> goals) async {
-  final selectedItem = await FlutterNativeSelect.openSelect(
-    items: goals.map<NativeSelectItem>((String goal) {
-      final String labelText = goal.replaceAll("#", "");
-      return NativeSelectItem(value: goal, label: labelText);
-    }).toList(),
-  );
-  
-  return selectedItem;
 }
