ba5570ca1c9822c2a04897097fa48ddb90ed9c75
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-08-04 17:16:59 +0100

    build

--- lib/model/journey.dart
+++ lib/model/journey.dart
@@ -1,6 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 
-/// This allows the `Goal` class to access private members in
+/// This allows the `Journey` class to access private members in
 /// the generated file. The value for this is *.g.dart, where
 /// the star denotes the source file name.
 part 'journey.g.dart';
@@ -17,13 +17,13 @@ class Journey {
   @JsonKey(required: true, name: 'bullet_type')
   int bulletType;
 
-  /// A necessary factory constructor for creating a new Goal instance
-  /// from a map. Pass the map to the generated `_$GoalFromJson()` constructor.
-  /// The constructor is named after the source class, in this case, Goal.
+  /// A necessary factory constructor for creating a new Journey instance
+  /// from a map. Pass the map to the generated `_$JourneyFromJson()` constructor.
+  /// The constructor is named after the source class, in this case, Journey.
   factory Journey.fromJson(Map<String, dynamic> json) => _$JourneyFromJson(json);
 
   /// `toJson` is the convention for a class to declare support for serialization
   /// to JSON. The implementation simply calls the private, generated
   /// helper method `_$JourneyFromJson`.
-  Map<String, dynamic> toJson() => _$JourneyFromJson(this);
+  Map<String, dynamic> toJson() => _$JourneyToJson(this);
 }
