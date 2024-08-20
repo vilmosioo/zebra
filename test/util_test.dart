c9a586ad66d5056fa9fe5a06d50dd8e80e23a707
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-08-20 17:47:35 +0100

    Added a small unit test (#33)
    * Added mocktail
    
    * Added small test

--- test/util_test.dart
+++ test/util_test.dart
@@ -4,13 +4,24 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:zebra/common/util.dart';
 
+class MockImport extends Mock implements FilePickerResult {}
 
 void main() {
-  test('Util functions', () async {
-    expect(await getAndParseFinchExport(null), {});
+  final mockImport = MockImport();
+
+  group('Util functions', () {
+    test('Parse null import correctly', () async {
+      expect(await getAndParseFinchExport(null), {});
+    });
+
+    test('Parse empty import correctly', () async {
+      when(() => mockImport.files).thenReturn([]);
+      expect(await getAndParseFinchExport(mockImport), {});
+    });
   });
 }
