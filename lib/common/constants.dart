cf94477f80229d3dce82178be58f596ac6234f78
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-08-21 16:18:17 +0100

    Hive box should not use dynamic types (#36)
    * Hive box should not use dynamic
    
    * fix unit test

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../model/report.dart';
+
 const zebraBox = "ZebraBox";
+
+ValueListenable<Box<List<Report>>> getZebraBox() {
+  return Hive.box<List<Report>>(zebraBox).listenable();
+}
