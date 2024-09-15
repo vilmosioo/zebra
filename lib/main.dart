7a3bb4703980fd506b26e3c515fc62d89167a18b
Vilmos Ioo <vilmosioo@users.noreply.github.com>
2024-09-15 13:39:01 +0100

    Add home navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 
 import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
 import "package:hive_flutter/hive_flutter.dart";
 
 import "common/constants.dart";
 import "model/main.dart";
 import "model/report.dart";
 import "widgets/home.dart";
+
+// GoRouter configuration
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => const ZebraHomePage(),
+    ),
+  ],
+);
 
 void main() async {
   Hive.registerAdapter(ReportAdapter()); 
@@ -20,12 +31,17 @@ class ZebraApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return MaterialApp(
       title: "Zebra",
+=======
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Zebra',
+>>>>>>> 4a56a16 (Add home navigation)
       theme: ThemeData(
         primaryColor: const Color.fromARGB(100, 65, 180, 255),
       ),
-      home: const ZebraHomePage(),
     );
   }
 }
